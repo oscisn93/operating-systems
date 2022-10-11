1ee2323198d3d809d35b829265a37d970e220b1e
Oscar <ocisneros1@csu.fullerton.edu>
2022-10-10 22:14:21 -0700

    deleted unnecessary files

--- dining_philosophers/main.c
+++ dining_philosophers/main.c
@@ -1,32 +1,25 @@
 // Created by Oscar Cisneros on 10/10/22.
 //
-
 #include <semaphore.h>
 #include <stdlib.h>
 #include <pthread.h>
 
+sem_t chopstick[5];
+
+void philosopher(int pos) {
+    sem_wait(&chopstick[0]);
+    sem_wait(&chopstick[1]);
+    printf("Philosopher %d is eating...");
+    sem_post(&chopstick[0]);
+    sem_post(&chopstick[1]);
+    print("Philosopher %d is thinking...");
+}
+
 int main(int argc, char *argv[]) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    int i;
+    for (i = 0; i < 5; i++)
+        sem_init(&chopstick[i], 0, 1);
+    
     return 0;
 }
