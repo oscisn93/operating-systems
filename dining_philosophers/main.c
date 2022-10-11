43ff7c42b71981f80a16da8568f3ef0168a4e444
Oscar <ocisneros1@csu.fullerton.edu>
2022-10-10 21:30:40 -0700

    added initial files

---
+++ dining_philosophers/main.c
@@ -0,0 +1,32 @@
+// Created by Oscar Cisneros on 10/10/22.
+//
+
+#include <semaphore.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char *rgv[]) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
